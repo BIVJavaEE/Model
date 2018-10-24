ecee4b908b377fea0b7e629d77a982a26522027e
batnyu <vrignaudbaptiste@gmail.com>
2018-10-24 15:07:51 +0200

    refactor: simplifying the model to meet the requirements of the product owner

--- src/main/java/entity/Alert.java
+++ src/main/java/entity/Alert.java
@@ -11,7 +11,29 @@ public class Alert {
     @Column(unique = true, nullable = false, updatable = false, insertable = false)
     private String id;
 
+    @OneToOne
+    private Sensor sensor;
+
+    @Basic(optional = false)
     private String name;
+
+    @Basic(optional = false)
+    private String criticity;
+
+    @Basic(optional = false)
+    private long timestamp;
+
+    @Basic(optional = false)
+    private boolean read;
+
+    @Basic(optional = false)
+    private long beginDate;
+
+    @Basic(optional = false)
+    private long endDate;
+
+    @Basic(optional = false)
+    private long treshold;
 
     public String getId() {
         return id;
@@ -21,4 +43,67 @@ public class Alert {
         this.id = id;
     }
 
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCriticity() {
+        return criticity;
+    }
+
+    public void setCriticity(String criticity) {
+        this.criticity = criticity;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public long getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(long beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public long getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(long endDate) {
+        this.endDate = endDate;
+    }
+
+    public long getTreshold() {
+        return treshold;
+    }
+
+    public void setTreshold(long treshold) {
+        this.treshold = treshold;
+    }
 }
